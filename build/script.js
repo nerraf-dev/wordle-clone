510ce0100d5701bc7c952484c7ec1bb736ae1796
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2022-04-09 20:54:01 +0200

    animate

--- build/script.js
+++ build/script.js
@@ -55,6 +55,7 @@ function insertLetter(pressedKey){
 
     let row = document.getElementsByClassName("letter-row")[6 - guessingRemaining];
     let box = row.children[nextLetter];
+    animateCSS(box, "pulse")
     box.textContent = pressedKey;
     box.classList.add("filled-box");
     currentGuess.push(pressedKey);
@@ -114,13 +115,14 @@ function checkGuess(){
 
         let delay = 250 * i;
         setTimeout(() => {
+            animateCSS(box, 'flipInX')
             //shade box
             box.style.backgroundColor = letterColour;
             shadeKeyBoard(letter, letterColour);
         }, delay)
     }
     if(guessString === correctGuessString){
-        toastr.success("You guess right!");
+        toastr.success("Well done you guessed right!");
         guessingRemaining = 0;
         return;
     } else {
@@ -165,3 +167,20 @@ document.getElementById("keyboard").addEventListener("click",(e) =>{
     }
     document.dispatchEvent(new KeyboardEvent("keyup", {'key': key}));
 })
+
+const animateCSS = (element, animation, prefix = 'animate__') => 
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        // const node = document.querySelector(element);
+        const node = element;
+        node.style.setProperty('--animate-duration','0.3s');
+        node.classList.add(`${prefix}animated`, animationName);
+
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`, animationName);
+            resolve('Animation ended');
+        }
+        node.addEventListener('animationend', handleAnimationEnd, {once: true});
+    })
