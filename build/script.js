0593867f42476317d0dd37f989196784d0dea357
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2022-04-09 20:07:40 +0200

    keyup event for kb entry

--- build/script.js
+++ build/script.js
@@ -24,3 +24,24 @@ function initBoard(){
 }
 
 initBoard();
+
+document.addEventListener("keyup",(e) => {
+    if(guessingRemaining === 0){
+        return;
+    }
+    let pressedKey = String(e.key);
+    if(pressedKey === "Backspace" && nextLetter !=0){
+        deleteLetter()
+        return;
+    }
+    if(pressedKey === "Enter"){
+        checkGuess();
+        return;
+    }
+    let found = pressedKey.match(/[a-z]/gi);
+    if(!found || found.length > 1){
+        return;
+    } else {
+        insertLetter(pressedKey);
+    }
+})
